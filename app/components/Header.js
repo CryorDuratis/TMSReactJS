6cc8f838839f923aaaaf0d6e95cade75b5dd0ad3
bryce <br0001ei@e.ntu.edu.sg>
2023-11-29 21:48:01 +0800

    finished header static css

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,7 +5,7 @@ import HeaderLoggedIn from "./HeaderLoggedIn"
 
 function Header() {
   return (
-    <Container class={"header-bar padding-sm header-bar-colour"} height={"3rem"} width={"100vw"}>
+    <Container class={"header-bar padding-sm header-bar-colour colr-dark"} height={"80px"}>
       <Link to="/" className="logo">
         {/* <img src="logo.png" /> */}
         KANBAN
--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -4,14 +4,13 @@ import Container from "./Container"
 function HeaderLoggedIn(props) {
   return (
     <Container class={"header-bar"}>
-      <button>
-        <img src="profile.png" />
-        <h1>username &#9660;</h1>
+      <button className="transp-btn">
+        <img src="profile.png" className="avatar" />
+        Username &#9660;
       </button>
-
-      <button>
-        <img src="logout.png" />
-        <h3>log out</h3>
+      <button className="transp-btn">
+        <img src="logout.png" className="icon" />
+        Log out
       </button>
     </Container>
   )
