99ad82cf47ebcb8d16e2c28fc4472f2b863ef16c
Bryce <bryce.tan.jw@gmail.com>
2023-12-11 18:03:24 +0800

    added app grid header

--- app/components/AppList.js
+++ app/components/AppList.js
@@ -1,7 +1,52 @@
-import React from "react"
+// import node modules
+import React, { useContext, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import Axios from "axios"
+
+// import components
 import Container from "../templates/Container"
+import StateContext from "../StateContext"
+import DispatchContext from "../DispatchContext"
 
 function AppList() {
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
+  const navigate = useNavigate()
+
+  // managing rendering
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState("")
+  // check user group
+  // managing editing
+  const [updateFlag, setUpdateFlag] = useState(false)
+  // managing creating
+
+  // display app information on first load, and on exit create.edit view
+  useEffect(() => {
+    const fetchApps = async () => {
+      try {
+        // Make authorization request to the server
+        const token = Cookies.get("token")
+        var response = await Axios.post("/apps/getall", { groupname: "admin", token })
+
+        // if not logged in
+        if (response.data.unauth) {
+          if (response.data.unauth === "login") {
+            appDispatch({
+              type: "logout",
+              message: "Logged out"
+            })
+            navigate("/login")
+          } else if (response.data.unauth === "role") {
+            appDispatch({ type: "btoast", message: "Unauthorized page, redirecting to home" })
+            navigate("/")
+          }
+          return
+        }
+      } catch (error) {}
+    }
+  }, [updateFlag])
+
   return (
     <Container class="bgclr-light1 content-container">
       <div className="flex-row" style={{ justifyContent: "space-between", whiteSpace: "nowrap" }}>
