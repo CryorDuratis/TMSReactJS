f5d00d4e152c413b8ce3101bc77a96177c939aa4
Bryce <bryce.tan.jw@gmail.com>
2023-12-12 14:17:34 +0800

    applist loop made

--- app/components/AppList.js
+++ app/components/AppList.js
@@ -7,6 +7,7 @@ import Axios from "axios"
 import Container from "../templates/Container"
 import StateContext from "../StateContext"
 import DispatchContext from "../DispatchContext"
+import Cookies from "js-cookie"
 
 function AppList() {
   const appState = useContext(StateContext)
@@ -14,9 +15,10 @@ function AppList() {
   const navigate = useNavigate()
 
   // managing rendering
-  const [isLoading, setIsLoading] = useState(true)
   const [error, setError] = useState("")
-  // check user group
+  const [CAButton, setCAButton] = useState(false)
+  // initial data
+  const [appList, setAppList] = useState([])
   // managing editing
   const [updateFlag, setUpdateFlag] = useState(false)
   // managing creating
@@ -24,53 +26,75 @@ function AppList() {
   // display app information on first load, and on exit create.edit view
   useEffect(() => {
     const fetchApps = async () => {
+      console.log("fetchapps is loaded")
       try {
         // Make authorization request to the server
         const token = Cookies.get("token")
-        var response = await Axios.post("/apps/getall", { groupname: "admin", token })
+        var response = await Axios.post("/app/getall", { groupname: "Project Lead", token })
+        console.log("response: ", response)
 
         // if not logged in
         if (response.data.unauth) {
+          console.log("user is unauth")
           if (response.data.unauth === "login") {
             appDispatch({
               type: "logout",
               message: "Logged out"
             })
             navigate("/login")
-          } else if (response.data.unauth === "role") {
-            appDispatch({ type: "btoast", message: "Unauthorized page, redirecting to home" })
-            navigate("/")
           }
+          console.log("cabutton set to false")
+          setCAButton(false)
           return
+        } else {
+          console.log("cabutton set to true")
+          setCAButton(true)
         }
-      } catch (error) {}
+      } catch (error) {
+        console.log("error: ", error)
+      }
     }
+    fetchApps()
   }, [updateFlag])
+
+  // app card component for easy rendering
+  const AppCard = props => {
+    const { appacro, startdate, enddate } = props.app
+    return (
+      <div className="app-card">
+        <span className="form-acronym">{appacro}</span>
+        <span className="form-startdate">{startdate}</span>
+        <span className="form-enddate">{enddate}</span>
+        <button className="form-details gobutton">View Details</button>
+      </div>
+    )
+  }
 
   return (
     <Container class="bgclr-light1 content-container">
       <div className="flex-row" style={{ justifyContent: "space-between", whiteSpace: "nowrap" }}>
         <h2>App List</h2>
-
-        <button type="button" className="gobutton">
-          Create App
-        </button>
+        {CAButton && (
+          <button type="button" className="gobutton">
+            Create App
+          </button>
+        )}
       </div>
-
       <Container class="list-container">
         <div className="app-grid-header">
           <strong>App Acronym</strong>
           <strong>App Start Date</strong>
           <strong>App End Date</strong>
         </div>
-        <div className="list-card-container">
-          <div className="app-card">
-            <span className="form-acronym">hi</span>
-            <span className="form-startdate">hi</span>
-            <span className="form-enddate">hi</span>
-            <button className="form-details gobutton">View Details</button>
-          </div>
-        </div>
+        {appList.length > 0 ? (
+          appList.map((app, index) => (
+            <div key={index} className="list-card-container">
+              <AppCard app={app} />
+            </div>
+          ))
+        ) : (
+          <p>No apps found. Create an app to begin.</p>
+        )}
       </Container>
     </Container>
   )
